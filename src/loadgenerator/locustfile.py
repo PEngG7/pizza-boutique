f8782e89e322e3ec70db176ace878c20764fc358
huaning-yang <huaning.y@gmail.com>
2023-07-18 19:43:27 +0200

    Update loadgenerator

--- src/loadgenerator/locustfile.py
+++ src/loadgenerator/locustfile.py
@@ -1,82 +1,85 @@
-#!/usr/bin/python
-#
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import grpc
+import time
+from locust import TaskSet, User, between, task, events
+from locust.exception import LocustError
+from grpc_interceptor import ClientInterceptor
+import demo_pb2
+import demo_pb2_grpc
+from typing import Any, Callable
 
-import random
-from locust import HttpUser, TaskSet, between
+class LocustInterceptor(ClientInterceptor):
+    def __init__(self, environment, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.env = environment
 
-products = [
-    '0PUK6V6EV0',
-    '1YMWWN1N4O',
-    '2ZYFJ3GM2N',
-    '66VCHSJNUP',
-    '6E92ZMYYFZ',
-    '9SIQT8TOJO',
-    'L9ECAV7KIM',
-    'LS4PSXUNUM',
-    'OLJCESPC7Z']
+    def intercept(
+        self,
+        method: Callable,
+        request_or_iterator: Any,
+        call_details: grpc.ClientCallDetails,
+    ):
+        response = None
+        exception = None
+        start_perf_counter = time.perf_counter()
+        response_length = 0
+        try:
+            response = method(request_or_iterator, call_details)
+            response_length = response.result().ByteSize()
+        except grpc.RpcError as e:
+            exception = e
 
-def index(l):
-    l.client.get("/")
+        self.env.events.request.fire(
+            request_type="grpc",
+            name=call_details.method,
+            response_time=(time.perf_counter() - start_perf_counter) * 1000,
+            response_length=response_length,
+            response=response,
+            context=None,
+            exception=exception,
+        )
+        return response
 
-def setCurrency(l):
-    currencies = ['EUR', 'USD', 'JPY', 'CAD']
-    l.client.post("/setCurrency",
-        {'currency_code': random.choice(currencies)})
 
-def browseProduct(l):
-    l.client.get("/product/" + random.choice(products))
+class GrpcUsers(User):
+    abstract = True
+    stub_class = None
 
-def viewCart(l):
-    l.client.get("/cart")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for attr_value, attr_name in ((self.host, "host"), (self.stub_class, "stub_class")):
+            if attr_value is None:
+                raise LocustError(f"You must specify the {attr_name}.")
 
-def addToCart(l):
-    product = random.choice(products)
-    l.client.get("/product/" + product)
-    l.client.post("/cart", {
-        'product_id': product,
-        'quantity': random.choice([1,2,3,4,5,10])})
+        self._channel = grpc.insecure_channel("trackingservice:50052")
+        interceptor = LocustInterceptor(environment=self.environment)
+        self._channel = grpc.intercept_channel(self._channel, interceptor)
 
-def checkout(l):
-    addToCart(l)
-    l.client.post("/cart/checkout", {
-        'email': 'someone@example.com',
-        'street_address': '1600 Amphitheatre Parkway',
-        'zip_code': '94043',
-        'city': 'Mountain View',
-        'state': 'CA',
-        'country': 'United States',
-        'credit_card_number': '4432-8015-6152-0454',
-        'credit_card_expiration_month': '1',
-        'credit_card_expiration_year': '2039',
-        'credit_card_cvv': '672',
-    })
+        self.stub = self.stub_class(self._channel)
 
-class UserBehavior(TaskSet):
+class GrpcUser(TaskSet):
+    @task
+    def grpc_request(self):
+        channel = grpc.insecure_channel("trackingservice:50052")
+        self.stub = demo_pb2_grpc.TrackingServiceStub(channel)
+        token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwb2xpY3kiOnsiYWxsb3dlZCI6eyJjaXR5IjpbInN0cmluZyJdLCJjb3VudHJ5IjpbInN0cmluZyJdLCJuYW1lIjpbInN0cmluZyJdLCJwaG9uZSI6WyJzdHJpbmciXSwic3RyZWV0X25hbWUiOlsic3RyaW5nIl0sInN0cmVldF9udW1iZXIiOlsic3RyaW5nIl0sInppcF9jb2RlIjpbInN0cmluZyJdfSwiZ2VuZXJhbGl6ZWQiOnsiY3JlZGl0X2NhcmRfY3Z2IjpbImludCIsIjMiXSwiY3JlZGl0X2NhcmRfZXhwaXJhdGlvbl95ZWFyIjpbImludCIsIjEwIl0sImNyZWRpdF9jYXJkX251bWJlciI6WyJzdHJpbmciLCI1Il19LCJub2lzZWQiOnsiYWdlIjpbImludCIsIkxhcGxhY2UiXSwiY3JlZGl0X2NhcmRfZXhwaXJhdGlvbl9tb250aCI6WyJpbnQiLCJMYXBsYWNlIl19LCJyZWR1Y2VkIjp7ImVtYWlsIjpbInN0cmluZyIsIjQiXX19LCJpc3MiOiJ0b2tlbkdlbmVyYXRvciIsImV4cCI6MTY4OTcwMTUzNn0.MXWdWqEIkCrM5QSbsAyIzT-lt6cX9KqPpWv8x_MaMMg8k6x7D8siQaMK2jp-cnVCBTkRFo_94euwnz1T5oI4Q4H5-ueiVxqWvzpGFa4mfCMmwf-xR1P9Xvfo04YO7QTfNrFStKCiukLKd5MT2R_nmsF-34auE3_-slLwvZz2lfRF7go6hqtih7YVOTIHNYHd82NPc-haqsqI8A_mjR6sGmNDuPsnXLq5NvIwBmLq5U6C3LVJxpgc63Y5ma7ACOXRSj4O3JqRN3xUpXyb5l38W2tRn4_FUtExvSLLxldzshuT1f4vRC6N0TT-M1RiPlyv60lshS7oYIm7Lt0mSa-4SQ'
+        request_msg = demo_pb2.TrackingRequest(
+            phone='030/1234567',
+            street_name='Marchstr',
+            street_number=23,
+            zip_code=10383,
+            city='Berlin',
+            country='Germany',
+            email='Test@mail.de',
+            name='Mustermann',
+            age=25
+        )
 
-    def on_start(self):
-        index(self)
+        metadata = [('authorization', token)]
 
-    tasks = {index: 1,
-        setCurrency: 2,
-        browseProduct: 10,
-        addToCart: 2,
-        viewCart: 3
-        # checkout: 1
-        }
+        response = self.stub.GetPersonaldata(request_msg, metadata=metadata)
+        print(response)
 
-class WebsiteUser(HttpUser):
-    tasks = [UserBehavior]
-    wait_time = between(1, 10)
+class GrpcUser(GrpcUsers):
+    stub_class = demo_pb2_grpc.TrackingServiceStub
+    tasks = [GrpcUser]
+    
